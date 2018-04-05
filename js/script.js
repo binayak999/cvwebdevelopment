9895c57ab386a504c1f28efbbb1e41baa5a1fd8e
Binayak Dhakal <binayakdhakal@Binayaks-MacBook-Pro.local>
2018-04-05 20:55:57 +0545

    new

---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function(){
+    var a = document.getElementsByClassName('.logo-menu');
+
+    function showMenu(){
+        // a.style.display('block');
+        console.log("hey");
+
+    }
+})();
